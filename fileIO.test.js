703f88d49dabcdfc08da7f1050a720fa3e49cafb
Rami Elsayed <ramielsayed123@outlook.com>
2022-06-22 21:00:28 +0100

    built texts mocking files

--- fileIO.test.js
+++ fileIO.test.js
@@ -1,19 +1,38 @@
 const FileIO = require("./fileIO");
+const fs = require('fs');
 
+jest.mock('fs');
 describe('FileIO', () => {
+  let fileIO;
+  beforeEach(() => {
+    jest.clearAllMocks();
+    fileIO = new FileIO();
+  })
   describe('Initialize', () => {
     test('should initialize new fileIO object', () => {
-      const fileIO = new FileIO();
 
       expect(fileIO).toEqual({});
       expect(fileIO).toBeInstanceOf(FileIO);
     })
   });
   describe('read', () =>  {
-    test('should call readFileSync with the expected arguments', () => {});
+    test('should call readFileSync with the expected arguments', () => {
+      fs.readFileSync.mockReturnValue('a mock');
+
+      const dataFromFile = fileIO.read('food.txt');
+
+      expect(dataFromFile).toEqual('a mock');
+      expect(fs.readFileSync).toHaveBeenCalledWith('food.txt', 'utf8');
+    });
   })
   describe('write', () => {
-    test('should call writeFileSync eith the expected arguments', () =>{});
+    test('should call writeFileSync eith the expected arguments', () =>{
+      fs.writeFileSync.mockReturnValue('success');
+      const result = fileIO.write('foo.text', 'This is a write mock');
+
+      expect(result).toEqual('success');
+      expect(fs.writeFileSync).toHaveBeenCalledWith('foo.text', 'This is a write mock');
+    });
     
   })
 });
